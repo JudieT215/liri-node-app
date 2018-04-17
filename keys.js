77bec8565f85709a68e5a1ccf36a4baedf68117c
Tran <Tran.Judie@gmail.com>
2018-04-17 18:11:47 -0400

    added some functions

--- keys.js
+++ keys.js
@@ -1,6 +1,7 @@
 
 require("dotenv").config()
 
+
 console.log('this is loaded');
 
 exports.twitter = {
--- liri.js
+++ liri.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
 var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fs = require('fs');
 
 
-// var spotify = new Spotify(keys.spotify);
+
+
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 //all node argv
 var arg1 = process.argv[2];
@@ -34,22 +38,78 @@ function myTweets() {
 
 console.log("my tweets wills display when i use the command my-tweets")
 
-var params = { screen_name: "JudieT267" };
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
-    console.log(tweets);
-  })
 
+
+
+client.get("search/tweets", { q: "JudieT267", count: 1}, function(
+  error,
+  tweets,
+  response
+) {
+  /// console.log(tweets.statuses.text);
+  if(error) {
+
+  console.log("ops")
+  }
+  else{
+        for(var i = 0; i<tweets.length; i++){
+        console.log("Created: " + tweets[i].created_at; + date.substring(0, 19))
+        }
+      }
+});
 }
 function spotifyThisSong(){
 
 console.log("my spotify will show up here when i use the command spotifyThisSong")
 
+spotify
+  .search({ type: "track", query: "All the Small Things", limit: 1 })
+  .then(function(response) {
+    console.log(JSON.stringify(response), null, 2);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
 }
 
-function movieThis() {
 
-    console.log("this will make movie information show up")
+
+function movieThis() {
+var queryUrl =
+  "http://www.omdbapi.com/?t=" + arg2 + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+// console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Released);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + "###");
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+
+    //        * Title of the movie.
+    //    * Year the movie came out.
+    //    * IMDB Rating of the movie.
+    //    * Rotten Tomatoes Rating of the movie.
+    //    * Country where the movie was produced.
+    //    * Language of the movie.
+    //    * Plot of the movie.
+    //    * Actors in the movie.
+    //  ```
+  }
+});
 
 }
 
